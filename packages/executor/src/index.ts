00a84890109d6e99047a86f7fabab704a03ce27b
LabnexDev <labnexcontact@gmail.com>
2025-06-15 13:14:01 -0500

    fix(executor): reference CLI dist and adjust tsconfig; update Dockerfile build order

--- packages/executor/src/index.ts
+++ packages/executor/src/index.ts
@@ -1,4 +1,4 @@
 // Temporary executor package that re-exports implementations from CLI package.
-export { LocalBrowserExecutor } from '../../cli/src/localBrowserExecutor';
-export { TestStepParser } from '../../cli/src/testStepParser';
-export * from '../../cli/src/lib/testTypes'; 
+export { LocalBrowserExecutor } from '../../cli/dist/localBrowserExecutor';
+export { TestStepParser } from '../../cli/dist/testStepParser';
+export * from '../../cli/dist/lib/testTypes'; 
