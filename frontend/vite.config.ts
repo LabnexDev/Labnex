cba435396b7be7d495829cb6c12925dea298325c
LabnexDev <labnexcontact@gmail.com>
2025-06-21 20:01:31 -0500

    updated AI voice page for speech

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteCompression from 'vite-plugin-compression';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteCompression({ algorithm: 'brotliCompress', deleteOriginFile: false }), viteCompression({ algorithm: 'gzip', deleteOriginFile: false })],
+  plugins: [react(), viteCompression({ algorithm: 'brotliCompress', deleteOriginFile: false }), viteCompression({ algorithm: 'gzip', deleteOriginFile: false }), basicSsl()],
   server: {
+    https: true,
     port: 5173,
     proxy: {
       '/api': {
